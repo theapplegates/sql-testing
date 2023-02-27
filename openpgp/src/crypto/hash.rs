b86ae96059f4560258babf4657ffbb426073457e
Justus Winter <justus@sequoia-pgp.org>
2023-02-27 11:28:54 +0100

    openpgp: Fix logging data written to the hash dumper.

--- openpgp/src/crypto/hash.rs
+++ openpgp/src/crypto/hash.rs
@@ -310,7 +310,8 @@ impl Digest for HashDumper {
 
 impl io::Write for HashDumper {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.hasher.write(buf)
+        self.update(buf);
+        Ok(buf.len())
     }
 
     fn flush(&mut self) -> io::Result<()> {
