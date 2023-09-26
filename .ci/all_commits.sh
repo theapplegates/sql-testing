ea31e87029288bfe5897b1dd6a02cdc291946c9e
Neal H. Walfield <neal@pep.foundation>
2023-09-26 10:46:04 +0200

    ci: Check commits leading to the merge base.
      - The `all_commits` check rebases the branch being tested onto main.
        This is brittle when the merge request is relative to some very
        old commit, as is the case when updating an old release.  In these
        cases, the changes aren't even intended to be rebased on main so
        rebasing them on main makes no sense.
    
      - Change the all_commits check to check commits up to the merge
        base.
    
      - Also increase the script's verbosity so it is clearer what is
        being checked.

--- .ci/all_commits.sh
+++ .ci/all_commits.sh
@@ -9,14 +9,41 @@
 git config user.name "C.I. McTestface"
 git config user.email "ci.mctestface@example.com"
 
+# We are only interested in fast-forward merges.  If main is not an
+# ancestor of HEAD, then we're not doing a fast-forward merge.
+if ! git merge-base --is-ancestor origin/main HEAD
+then
+    echo "***"
+    echo "*** WARNING: main cannot be fast-forwarded to HEAD"
+    echo "***"
+fi
+
+MERGE_BASE=$(git merge-base origin/main HEAD)
+if test "x$MERGE_BASE" = x
+then
+    echo "Failed to find a common ancestor for main and HEAD."
+    exit 1
+fi
+
+# Show the commit graph from the merge base to HEAD and from the merge
+# base to main.
+git --no-pager log --pretty=oneline --graph \
+    HEAD origin/main --boundary ^$MERGE_BASE
+
 # If the previous commit already is on main we're done.
 git merge-base --is-ancestor HEAD~ origin/main &&
   echo "All commits tested already" &&
   exit 0
 
+# Show what we are going to check.
+echo "Checking:"
+git --no-pager log --pretty=oneline $MERGE_BASE..HEAD~
+
 # Leave out the last commit - it has already been checked.
 git checkout HEAD~
-git rebase origin/main \
+# Now, run cargo test on each commit.  Also fail if it leaves the tree
+# dirty.
+git rebase $MERGE_BASE \
            --exec 'echo ===; echo ===; echo ===; git log -n 1;' \
            --exec 'cargo test -p sequoia-openpgp' \
            --exec 'git diff --exit-code' &&
