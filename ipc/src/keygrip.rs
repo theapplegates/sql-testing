6104dc8fe31f0f07a61877502ff7442892d0d35c
Neal H. Walfield <neal@sequoia-pgp.org>
2024-05-16 15:53:41 +0200

    ipc: Extend Keygrip::of to support brainpoolP384r1 curves.
      - `Keygrip::of` is missing support for brainpoolP384r1 curves.
    
      - Add it.

--- ipc/src/keygrip.rs
+++ ipc/src/keygrip.rs
@@ -271,10 +271,31 @@ fn ecc_param(curve: &Curve, i: usize) -> Result<MPI> {
         (Cv25519, 5) => "0x08",
 
         (_, _) => {
-            return Err(Error::InvalidArgument(
-                format!("Invalid or unknown curve parameters: \
-                         curve: {}, i: {}",
-                        curve, i)).into());
+            let error = || {
+                Err(Error::InvalidArgument(
+                    format!("Invalid or unknown curve parameters: \
+                             curve: {}, i: {}",
+                            curve, i)).into())
+            };
+
+            const BRAINPOOL_P384_OID: &[u8] =
+                &[0x2B, 0x24, 0x03, 0x03, 0x02, 0x08, 0x01, 0x01, 0x0B];
+
+            if curve.oid() == BRAINPOOL_P384_OID {
+                match i {
+                    0 => "0x8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b412b1da197fb71123acd3a729901d1a71874700133107ec53",
+                    1 => "0x7bc382c63d8c150c3c72080ace05afa0c2bea28e4fb22787139165efba91f90f8aa5814a503ad4eb04a8c7dd22ce2826",
+                    2 => "0x04a8c7dd22ce28268b39b55416f0447c2fb77de107dcd2a62e880ea53eeb62d57cb4390295dbc9943ab78696fa504c11",
+                    4 => "0x8cb91e82a3386d280f5d6f7e50e641df152f7109ed5456b31f166e6cac0425a7cf3ab6af6b7fc3103b883202e9046565",
+                    3 => "0x04\
+                          1d1c64f068cf45ffa2a63a81b7c13f6b8847a3e77ef14fe3db7fcafe0cbd10e8e826e03436d646aaef87b2e247d4af1e\
+                          8abe1d7520f9c2a45cb1eb8e95cfd55262b70b29feec5864e19c054ff99129280e4646217791811142820341263c5315",
+                    5 => "0x01",
+                    _ => return error(),
+                }
+            } else {
+                return error();
+            }
         }
     };
 
