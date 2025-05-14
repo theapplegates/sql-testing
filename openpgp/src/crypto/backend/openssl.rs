b6121e9439ca2d6d176ed544c81b8bf717f1327e
Justus Winter <justus@sequoia-pgp.org>
2025-05-14 13:59:54 +0200

    openpgp: Test for AEAD modes at runtime when using OpenSSL.
      - Some modes may be compiled in, but disabled at runtime, for
        example by putting OpenSSL into FIPS mode on Fedora and RedHat
        systems.  This mirrors what we do for symmetric algorithms.

--- openpgp/src/crypto/backend/openssl.rs
+++ openpgp/src/crypto/backend/openssl.rs
@@ -37,12 +37,30 @@ impl AEADAlgorithm {
     }
 
     pub(crate) fn is_supported_by_backend(&self) -> bool {
-        match self {
+        // First, check whether support is compiled in or not.
+        (match self {
             AEADAlgorithm::EAX => false,
             AEADAlgorithm::OCB => cfg!(not(osslconf = "OPENSSL_NO_OCB")),
             AEADAlgorithm::GCM => true,
             AEADAlgorithm::Private(_) |
             AEADAlgorithm::Unknown(_) => false,
+        }) && {
+            // Then, check whether it has been disabled at runtime,
+            // for example by putting OpenSSL into FIPS mode.
+            use std::sync::OnceLock;
+
+            static MODES: [OnceLock<bool>; 3] = [const { OnceLock::new() }; 3];
+            MODES.get(u8::from(*self) as usize - 1)
+                .map(|cell| cell.get_or_init(|| {
+                    let algo = SymmetricAlgorithm::AES128;
+                    let key = [0; 16];
+                    let nonce = &key[..self.nonce_size().unwrap_or(16)];
+                    self.context(algo, &key, &[], nonce,
+                                 crate::crypto::aead::CipherOp::Encrypt)
+                        .is_ok()
+                }))
+                .cloned()
+                .unwrap_or(false)
         }
     }
 
